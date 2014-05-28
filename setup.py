8f831b17603c39f23ae5156d3f66d66ca6c3a07f
Ben Thompson <t.ben.thompson@gmail.com>
2014-05-28 15:58:49 -0400

    Forgot to uncomment the f2py section in setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from numpy.distutils.core import setup, Extension
 # -O0 means compile with no optimization, try -O3 for blazing speed
 compile_args = ['-O2']
 ext = []
-# ext.append(Extension('DC3D',
-#                   sources = ['okada_wrapper/DC3D.f',
-#                              'okada_wrapper/DC3D.pyf'],
-#                   extra_compile_args=compile_args))
+ext.append(Extension('DC3D',
+                  sources = ['okada_wrapper/DC3D.f',
+                             'okada_wrapper/DC3D.pyf'],
+                  extra_compile_args=compile_args))
 
 setup(
    name = "okada_wrapper",
