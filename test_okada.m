d4fa2816e0dde6bf6c20854b733ce20d3a6cab24
Ben Thompson <t.ben.thompson@gmail.com>
2014-08-21 13:51:14 -0400

    Added finite rectangular source wrapper.

--- test_okada.m
+++ test_okada.m
@@ -1,10 +1,11 @@
 source_depth = 3.0;
+square = 0.5;
 obs_depth = 3.0;
 poisson_ratio = 0.25;
 mu = 1.0;
 dip = 90;
 lambda = 2 * mu * poisson_ratio / (1 - 2 * poisson_ratio);
-alpha = (lambda + mu) / (lambda + 2 * mu);
+alpha = (lambda + mu) / (lambda + 2 * mu)
 x = linspace(-1, 1, 100);
 y = linspace(-1, 1, 100);
 ux = zeros(100, 100);
@@ -20,6 +21,25 @@ for i = 1:100
 end
 
 contourf(x, y, log(abs(ux)))
+xlabel('x')
+ylabel('y')
+colorbar()
+
+figure()
+
+for i = 1:100
+    for j = 1:100
+        [success, u, grad_u] = DC3Dwrapper(alpha,...
+                                           [x(i), y(j), -obs_depth],...
+                                           source_depth, dip,...
+                                           [-0.7, 0.7],...
+                                           [-0.7, 0.7],...
+                                           [1.0, 0.0, 0.0]);
+        ux(i, j) = u(1);
+    end
+end
+
+contourf(x, y, ux)
 xlabel('x')
 ylabel('y')
 colorbar()
