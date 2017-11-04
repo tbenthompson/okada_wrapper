cb2e29dc1364a7f0da8e84a377582bff337305af
Ben Thompson <t.ben.thompson@gmail.com>
2017-11-03 23:31:43 -0400

    17.11.03, fixed python 2 imports

--- okada_wrapper/__init__.py
+++ okada_wrapper/__init__.py
@@ -1,1 +1,6 @@
-from okada_wrapper.okada_wrapper import dc3d0wrapper, dc3dwrapper
+import sys
+
+if (sys.version_info > (3, 0)):
+    from okada_wrapper.okada_wrapper import dc3d0wrapper, dc3dwrapper
+else:
+    from okada_wrapper import dc3d0wrapper, dc3dwrapper
